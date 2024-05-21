748613eb85ffa65222d753c9ad2d587463bc080a
CunAnt <phanhoangtai15042001@gmail.com>
2024-05-21 13:51:03 +0900

    Refactor code: create router, app/controller folders

---
+++ src/app/controllers/NewControllers.js
@@ -0,0 +1,12 @@
+class NewControllers {
+  // [GET] /news
+  index(req, res) {
+    res.render("new");
+  }
+  // [GET] /:slug
+  show(req, res) {
+    res.send("show pages");
+  }
+}
+
+module.exports = new NewControllers();
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars").engine;
+const route = require("./routes");
 const app = express();
 const port = 5000;
 
@@ -27,15 +28,7 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => res.render("home"));
-
-app.get("/new", (req, res) => res.render("new"));
-
-app.get("/search", (req, res) => res.render("search"));
-app.post("/search", (req, res) => {
-  console.log(req.body);
-  res.send("submit success");
-});
+route(app);
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
